cc83599779d93683322a8d422eabc549cfb13aa1
Tobias Faßbender <tobias-fassbender@gmx.de>
2016-11-14 15:22:54 +0100

    implemented building and hero frameworks

--- src/net/jfabricationgames/bunkers_and_badasses/game_character/Building.java
+++ src/net/jfabricationgames/bunkers_and_badasses/game_character/Building.java
@@ -2,13 +2,26 @@ package net.jfabricationgames.bunkers_and_badasses.game_character;
 
 public abstract class Building {
 	
-	protected int recruitedTroops;
+	protected int recruitableTroops;
 	protected int ammoMining;
 	protected int creditMining;
 	protected int eridiumMining;
-	
-	
+	protected int landMineVictims;
+	protected int additionalDefence;
+	protected int moveDistance;
 	
 	protected boolean attackable;
 	protected boolean badassTroopsRecruitable;
+	
+	public Building() {
+		recruitableTroops = 1;
+		ammoMining = 0;
+		creditMining = 0;
+		eridiumMining = 0;
+		landMineVictims = 0;
+		additionalDefence = 0;
+		moveDistance = 1;
+		attackable = true;
+		badassTroopsRecruitable = false;
+	}
 }
--- src/net/jfabricationgames/bunkers_and_badasses/game_character/Hero.java
+++ src/net/jfabricationgames/bunkers_and_badasses/game_character/Hero.java
@@ -1,5 +1,16 @@
 package net.jfabricationgames.bunkers_and_badasses.game_character;
 
+import net.jfabricationgames.bunkers_and_badasses.game.Fight;
+
 public abstract class Hero {
 	
+	protected HeroEffectTime time;
+	
+	protected int attack;
+	protected int defence;
+	
+	protected int usedAttack;
+	protected int usedDefence;
+	
+	public abstract void executeFight(Fight fight);
 }
